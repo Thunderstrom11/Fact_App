e44b4118c2ff13c83f41dea0841ef4db25f10fe0
Diego Chamendy <dmchamendy@uamv.edu.ni>
2026-09-09 15:02:33 -0600

    Actualizacion: menuprincipal fxml, SceneManager, FacturacionApplication

--- src/main/java/ni/edu/uam/fact_app/application/FacturacionApplication.java
+++ src/main/java/ni/edu/uam/fact_app/application/FacturacionApplication.java
@@ -1,4 +1,20 @@
 package ni.edu.uam.fact_app.application;
 
-public class FacturacionApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FacturacionApplication extends Application {
+    @Override
+
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ni/edu/uam/facturacion/fxml/Menu-principal.fxml"));
+        stage.setTitle("Sistema de facturacion");
+        stage.setScene(new Scene(loader.load(), 900, 600));
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
